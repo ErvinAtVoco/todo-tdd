0b64e97f6ab107653ec398693678e3f331532a3f
ErvinAtVoco <ervin.tombak@voco.ee>
2024-01-25 19:17:58 +0200

    Created TodoController.createTodo unit test with data and HTTP mocks

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+import mongoose from "mongoose";
 const express = require('express');
 const app = express();
+
+mongoose.connect("mongodb+srv://ervintombak:0MTJOEpkQwWA1Iei@cluster0.yynfjjn.mongodb.net/");
+const database = mongoose.connection;
 
 app.get('/', (req,res) => {
     res.send('express test');
