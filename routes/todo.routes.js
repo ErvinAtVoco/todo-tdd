d5f69edfcc4a2f689b6644379ad55aafac282b9b
ErvinAtVoco <ervin.tombak@voco.ee>
2024-01-25 21:49:42 +0200

    Created TodoController.getTodos unit test and integration tests

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -3,5 +3,6 @@ const todoController = require("../controllers/todo.controller");
 const router = express.Router();
 
 router.post("/", todoController.createTodo);
+router.get('/', todoCtonroller.getTodos);
 
 module.exports = router;
